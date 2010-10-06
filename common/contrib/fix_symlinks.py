169133e16e316476272878f70980bc03db071dfb
Kris Maglione <maglione.k@gmail.com>
2010-10-06 15:01:32 -0400

    Add Mercurial hook hack to fix symlinks on Windows®.

---
+++ common/contrib/fix_symlinks.py
@@ -0,0 +1,20 @@
+from mercurial import util
+import os
+
+def fix_symlinks(repo, hooktype, parent1, **kwargs):
+    revert = hooktype in ('precommit', 'preupdate')
+    ctxt = repo[parent1]
+    for filename in ctxt:
+        file = ctxt[filename]
+        if 'l' in file.flags():
+            path = repo.wjoin(file.path())
+            try:
+                os.unlink(path)
+            except Exception, e:
+                print repr(e)
+            if revert:
+                repo.wwrite(file.path(), file.data(), '')
+            else:
+                target = os.path.join(os.path.dirname(path), file.data())
+                util.copyfiles(target, path, True)
+
