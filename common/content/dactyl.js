3f42eee0dbbc2c048321bf8434e7c8d898c5f275
Kris Maglione <maglione.k@gmail.com>
2010-10-14 03:34:09 -0400

    Fix Addon#userDisabled setter on FF36. Closes issue #82.

--- common/content/dactyl.js
+++ common/content/dactyl.js
@@ -1372,9 +1372,9 @@ const Dactyl = Module("dactyl", {
                         services.get("extensionManager").uninstallItem(this.id);
                     };
                     addon.appDisabled = false;
-                    addon.__defineGetter__("userDisabled", function () getRdfProperty(addon, "userDisabled") == "true");
+                    addon.__defineGetter__("userDisabled", function () getRdfProperty(addon, "userDisabled") === "true");
                     addon.__defineSetter__("userDisabled", function (val) {
-                        services.get("extensionManager")[val ? "enableItem" : "disableItem"](this.id);
+                        services.get("extensionManager")[val ? "disableItem" : "enableItem"](this.id);
                     });
 
                     return callback(addon);
