c9549cbd732766fce0f0aac383eecc5c79f29bef
Kris Maglione <maglione.k@gmail.com>
2011-01-18 14:10:28 -0500

    Re-enable tab numbers on FF36.

--- common/content/dactyl.js
+++ common/content/dactyl.js
@@ -1394,8 +1394,7 @@ var Dactyl = Module("dactyl", XPCOM(Ci.nsISupportsWeakReference, ModuleBase), {
                 /*
                 validator: function (opts) dactyl.has("Gecko2") ||
                     Option.validIf(!/[nN]/.test(opts), "Tab numbering not available in this " + config.host + " version")
-                */
-
+                 */
             }
         ].filter(function (group) !group.feature || dactyl.has(group.feature));
 
