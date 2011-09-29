6a433c69e35da00f1e3ce2028fc358ccd17a0f5e
Kris Maglione <maglione.k@gmail.com>
2011-09-29 01:57:05 -0400

    More manifest restructuring with Makefile rules to simplify the process.

--- binary/config.mk
+++ binary/config.mk
@@ -4,8 +4,11 @@ GECKO_MINOR ?= 0
 ABI_OS        := $(shell uname -s)
 ABI_ARCH      := $(shell uname -m)
 ABI_COMPILER  := gcc3
-ABI           ?= $(GECKO_MAJOR).$(GECKO_MINOR)-$(ABI_OS)_$(ABI_ARCH)-$(ABI_COMPILER)
+ABI_PLATFORM  ?= $(ABI_OS)_$(ABI_ARCH)-$(ABI_COMPILER)
+ABI           ?= $(GECKO_MAJOR).$(GECKO_MINOR)-$(ABI_PLATFORM)
 DEFINES        = -DGECKO_MAJOR=$(GECKO_MAJOR) -DGECKO_MINOR=$(GECKO_MINOR)
+
+LIBEXT	      ?= so
 
 SED := $(shell if [ "xoo" = x$$(echo foo | sed -E 's/f(o)/\1/' 2>/dev/null) ];	\
 	       then echo sed -E; else echo sed -r;				\
