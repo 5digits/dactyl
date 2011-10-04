b98c2e0906cbd09d73fd2e0fa1f35a2529ae72e7
Kris Maglione <maglione.k@gmail.com>
2011-10-04 03:08:00 -0400

    Argh. Fix incomplete commit.

--- common/content/commandline.js
+++ common/content/commandline.js
@@ -1381,7 +1381,7 @@ var CommandLine = Module("commandline", {
             if (this.context.waitingForTab || this.wildIndex == -1)
                 this.complete(true, true);
 
-            this.wildtypes = wildmode;
+            this.wildtypes = wildmode || options["wildmode"];
             let count = Math.abs(offset);
             let steps = Math.constrain(this.wildtypes.length - this.wildIndex,
                                        1, count);
