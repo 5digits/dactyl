a9ff4b908318f07498c82b108cae1c9b4af53203
Kris Maglione <maglione.k@gmail.com>
2013-04-06 14:33:17 -0700

    Turn off statusbar redrawing hack on 20+.

--- common/content/commandline.js
+++ common/content/commandline.js
@@ -261,7 +261,7 @@ var CommandWidgets = Class("CommandWidgets", {
         [this.commandbar.container, this.statusbar.container].forEach(check);
 
         // Work around a redrawing bug.
-        if (changed && config.haveGecko("16")) {
+        if (changed && config.haveGecko("16", "20")) {
             util.delay(function () {
                 // Urgh.
                 statusline.statusBar.style.paddingRight = "1px";
