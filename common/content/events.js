185922620310bda207202b83e78783e78a47cebe
Doug Kearns <dougkearns@gmail.com>
2015-02-24 23:49:06 +1100

    Fix issue #1270.

--- common/content/events.js
+++ common/content/events.js
@@ -475,7 +475,7 @@ var Events = Module("events", {
                         .toObject();
 
     outer:
-        for (let [, key] of iter(elements))
+        for (let key of elements)
             if (filters.some(([k, v]) => key.getAttribute(k) == v)) {
                 let keys = { ctrlKey: false, altKey: false, shiftKey: false, metaKey: false };
                 let needed = { ctrlKey: event.ctrlKey, altKey: event.altKey, shiftKey: event.shiftKey, metaKey: event.metaKey };
