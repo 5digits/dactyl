5digits/dactyl

commit 52993ec78106a775dbfd05e6a7f7a5c39aa9012d

Remove more unused vars.

common/content/commandline.js
@@ -1351,7 +1351,6 @@ var CommandLine = Module("commandline", {
                 return;
             }
 
-            let value = this.editor.selection.focusNode.textContent;
             this.saveInput();
 
             if (this.itemList.visible)
@@ -1787,8 +1786,6 @@ var CommandLine = Module("commandline", {
              function ({ self }) {
                  if (self.completions)
                      self.completions.tabTimer.flush();
-
-                 let command = commandline.command;
 
                  self.accepted = true;
                  return function () { modes.pop(); };

common/content/bookmarks.js
@@ -216,7 +216,6 @@ var Bookmarks = Module("bookmarks", {
      * use in completion functions.
      */
     get searchEngines() {
-        let searchEngines = [];
         let aliases = {};
         return iter(services.browserSearch.getVisibleEngines({})).map(function ([, engine]) {
             let alias = engine.alias;
@@ -430,7 +429,7 @@ var Bookmarks = Module("bookmarks", {
         const tags = {
             names: ["-tags", "-T"],
             description: "A comma-separated list of tags",
-            completer: function tags(context, args) {
+            completer: function tags(context) {
                 context.generate = function () Ary(b.tags
                                                    for (b of bookmarkcache)
                                                    if (b.tags))

common/content/editor.js
@@ -846,7 +846,6 @@ var Editor = Module("editor", XPCOM(Ci.nsIEditActionListener, ModuleBase), {
                     count = count || 1;
 
                     let caret = !dactyl.focusedElement;
-                    let controller = buffer.selectionController;
 
                     while (count-- && modes.main == modes.VISUAL) {
                         if (caret)

common/content/events.js
@@ -374,7 +374,6 @@ var Events = Module("events", {
                 commandline.quiet = quiet;
 
             for (let evt_obj of DOM.Event.parse(keys)) {
-                let now = Date.now();
                 let key = DOM.Event.stringify(evt_obj);
                 for (let type of values(["keydown", "keypress", "keyup"])) {
                     let evt = update({}, evt_obj, { type: type });
@@ -1109,7 +1108,6 @@ var Events = Module("events", {
         mappings.add([modes.COMMAND],
             ["<A-m>s", "<sleep>"], "Sleep for {count} milliseconds before continuing macro playback",
             function ({ command, count }) {
-                let now = Date.now();
                 dactyl.assert(count, _("error.countRequired", command));
                 if (events.feedingKeys)
                     util.sleep(count);

common/content/history.js
@@ -274,7 +274,7 @@ var History = Module("history", {
                         names: ["-sort", "-s"],
                         type: CommandOption.STRING,
                         description: "The sort order of the results",
-                        completer: function (context, args) {
+                        completer: function (context) {
                             context.compare = CompletionContext.Sort.unsorted;
                             return Ary.flatten([
                                 "annotation",

common/content/key-processors.js
@@ -68,7 +68,6 @@ var ProcessorStack = Class("ProcessorStack", {
         events.dbg("EXECUTE(" + this._result(result) + ", " + force + ") events:" + this.events.length
                    + " processors:" + this.processors.length + " actions:" + this.actions.length);
 
-        let processors = this.processors;
         let length = 1;
 
         if (force)

common/content/mow.js
@@ -35,7 +35,7 @@ var MOW = Module("mow", {
         events.listen(window, this, "windowEvents");
 
         modules.mow = this;
-        let fontSize = DOM(document.documentElement).style.fontSize;
+        //let fontSize = DOM(document.documentElement).style.fontSize;
         styles.system.add("font-size", "dactyl://content/buffer.xhtml",
                           // "body { font-size: " + fontSize + "; } \
                           "html|html > xul|scrollbar { visibility: collapse !important; }",

common/content/statusline.js
@@ -75,7 +75,6 @@ var StatusLine = Module("statusline", {
                 */$));
         }
 
-        let _commandline = "if (window.dactyl) return dactyl.modules.commandline";
         let prepend = [
             ["button", { id: "appmenu-button", label: "", image: "chrome://branding/content/icon16.png", highlight: "AppmenuButton", xmlns: "xul" }],
             ["toolbarbutton", { id: "appmenu-toolbar-button", label: "", image: "chrome://branding/content/icon16.png" }],

commit aee0c79752a2bc762d29f7d48f0ebf50568669f4

Fix commandline history.
Broken in 52993ec.

common/content/commandline.js
@@ -1787,6 +1787,8 @@ var CommandLine = Module("commandline", {
                  if (self.completions)
                      self.completions.tabTimer.flush();
 
+                 commandline.command;
+
                  self.accepted = true;
                  return function () { modes.pop(); };
              });